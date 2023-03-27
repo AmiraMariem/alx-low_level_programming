08744f90102ddaaaae439fcf2fce27572909a207
AmiraMariem <amiramohamed409@gmail.com>
2023-03-27 16:05:27 -0700

    amira

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,30 +1,10 @@
-k#include "main.h"
-#include <stdio.h>
-#include <time.h>
-
+#import "holberton.h"
 /**
- * main - generates random valid passwords
- * return: 0 (on success).
- *
+ * main -
+ * Return:
  */
-
 int main(void)
 {
-
-	int pass, sum;
-
-
-	srand(time(NULL));
-
-	sum = 0;
-	while (sum <= 2645)
-	{
-		pass = (rand()  %  128);
-
-		sum += pass;
-		printf("%c", pass);
-	}
-	printf("%c", 2772 - sum);
-
 	return (0);
 }
+
